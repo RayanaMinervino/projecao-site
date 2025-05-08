a3060d91111b6480552c72069c91ad45fce21c62
RayanaMinervino <minervinorayana@gmail.com>
2025-05-08 19:03:02 -0300

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+apt-get update && \
+    apt-get install -y curl gnupg && \
+    curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add - && \
+    curl https://packages.microsoft.com/config/debian/10/prod.list > /etc/apt/sources.list.d/mssql-release.list && \
+    apt-get update && \
+    ACCEPT_EULA=Y apt-get install -y msodbcsql17 unixodbc-dev
